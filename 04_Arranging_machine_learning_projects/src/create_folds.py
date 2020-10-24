53b65635ca422ee917124717bc8439e47a68ca3d
Peko1521 <47183987+Peko1521@users.noreply.github.com>
2020-10-25 02:56:55 +0900

    Create create_folds.py

---
+++ 04_Arranging_machine_learning_projects/src/create_folds.py
@@ -0,0 +1,33 @@
+# stratified-kfold for regression 
+import numpy as np 
+import pandas as pd 
+ 
+from sklearn import datasets 
+from sklearn import model_selection 
+
+if __name__ == "__main__":
+    # Read training data
+    df = pd.read_csv("../input/mnist_train.csv")
+    # map positive to 1 and negative to 0
+    # df.sentiment = df.sentiment.apply(
+    #     lambda x: 1 if x == "positive" else 0
+    # )
+    
+    # we create a new column called kfold and fill it with -1
+    df["kfold"] = -1
+
+    # the next step is to randomize the rows of the data
+    df = df.sample(frac=1).reset_index(drop=True)
+    
+    # fetch labels
+    y = df.target.values
+    
+    # initiate the kfold class from model_selection module
+    kf = model_selection.StratifiedKFold(n_splits=5)
+    
+    # fill the new kfold column
+    for f, (t_, v_) in enumerate(kf.split(X=df, y=y)):
+        df.loc[v_, 'kfold'] = f
+    
+    # save the new csv with kfold column
+    df.to_csv("../input/mnist_train_folds.csv", index=False)
